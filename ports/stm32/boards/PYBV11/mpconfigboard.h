3dfe5e14d604194aef7403513f4e5a11502346f0
czar <czarbalangue@gmail.com>
2024-09-11 11:54:46 +0100

    edited to cellag-board

--- ports/stm32/boards/PYBV11/mpconfigboard.h
+++ ports/stm32/boards/PYBV11/mpconfigboard.h
@@ -24,19 +24,19 @@
 #define MICROPY_HW_RTC_USE_US       (0)
 #define MICROPY_HW_RTC_USE_CALOUT   (1)
 
-// UART config
+// UART config cycler
 #define MICROPY_HW_UART1_NAME   "XB"
-#define MICROPY_HW_UART1_TX     (pin_B6)
-#define MICROPY_HW_UART1_RX     (pin_B7)
+#define MICROPY_HW_UART1_TX     (pin_B6)//pin_A9
+#define MICROPY_HW_UART1_RX     (pin_B7)//pin_A10
 #define MICROPY_HW_UART2_TX     (pin_A2)
 #define MICROPY_HW_UART2_RX     (pin_A3)
 #define MICROPY_HW_UART2_RTS    (pin_A1)
 #define MICROPY_HW_UART2_CTS    (pin_A0)
-#define MICROPY_HW_UART3_NAME   "YB"
-#define MICROPY_HW_UART3_TX     (pin_B10)
-#define MICROPY_HW_UART3_RX     (pin_B11)
-#define MICROPY_HW_UART3_RTS    (pin_B14)
-#define MICROPY_HW_UART3_CTS    (pin_B13)
+//#define MICROPY_HW_UART3_NAME   "YB"
+//#define MICROPY_HW_UART3_TX     (pin_B10)//disable
+//#define MICROPY_HW_UART3_RX     (pin_B11)
+//#define MICROPY_HW_UART3_RTS    (pin_B14)
+//#define MICROPY_HW_UART3_CTS    (pin_B13)
 #define MICROPY_HW_UART4_NAME   "XA"
 #define MICROPY_HW_UART4_TX     (pin_A0)
 #define MICROPY_HW_UART4_RX     (pin_A1)
@@ -44,25 +44,25 @@
 #define MICROPY_HW_UART6_TX     (pin_C6)
 #define MICROPY_HW_UART6_RX     (pin_C7)
 
-// I2C buses
+// I2C buses correct
 #define MICROPY_HW_I2C1_NAME "X"
-#define MICROPY_HW_I2C1_SCL (pin_B6)
-#define MICROPY_HW_I2C1_SDA (pin_B7)
+#define MICROPY_HW_I2C1_SCL (pin_B6)//b10
+#define MICROPY_HW_I2C1_SDA (pin_B7)//b11
 #define MICROPY_HW_I2C2_NAME "Y"
-#define MICROPY_HW_I2C2_SCL (pin_B10)
-#define MICROPY_HW_I2C2_SDA (pin_B11)
+#define MICROPY_HW_I2C2_SCL (pin_B10)//b8
+#define MICROPY_HW_I2C2_SDA (pin_B11)//b9
 
-// SPI buses
+// SPI buses correct
 #define MICROPY_HW_SPI1_NAME "X"
 #define MICROPY_HW_SPI1_NSS  (pin_A4) // X5
 #define MICROPY_HW_SPI1_SCK  (pin_A5) // X6
 #define MICROPY_HW_SPI1_MISO (pin_A6) // X7
 #define MICROPY_HW_SPI1_MOSI (pin_A7) // X8
 #define MICROPY_HW_SPI2_NAME "Y"
-#define MICROPY_HW_SPI2_NSS  (pin_B12) // Y5
-#define MICROPY_HW_SPI2_SCK  (pin_B13) // Y6
-#define MICROPY_HW_SPI2_MISO (pin_B14) // Y7
-#define MICROPY_HW_SPI2_MOSI (pin_B15) // Y8
+//#define MICROPY_HW_SPI2_NSS  (pin_B12) // Y5
+//#define MICROPY_HW_SPI2_SCK  (pin_B13) // Y6
+//#define MICROPY_HW_SPI2_MISO (pin_B14) // Y7
+//#define MICROPY_HW_SPI2_MOSI (pin_B15) // Y8
 
 // I2S buses
 #define MICROPY_HW_I2S2      (1)
@@ -92,14 +92,14 @@
 #define MICROPY_HW_LED_OFF(pin)     (mp_hal_pin_low(pin))
 
 // SD card detect switch
-#define MICROPY_HW_SDCARD_DETECT_PIN        (pin_A8)
+//#define MICROPY_HW_SDCARD_DETECT_PIN        (pin_A8)
 #define MICROPY_HW_SDCARD_DETECT_PULL       (GPIO_PULLUP)
 #define MICROPY_HW_SDCARD_DETECT_PRESENT    (GPIO_PIN_RESET)
 
 // USB config
 #define MICROPY_HW_USB_FS              (1)
-#define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
-#define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
+//#define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
+//#define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
 
 // MMA accelerometer config
 #define MICROPY_HW_MMA_AVDD_PIN     (pin_B5)
