f9fd97a724748f3432d6af1fce24a608b7738f3e
czar <czarbalangue@gmail.com>
2024-09-30 11:07:44 +0100

    wip

--- ports/stm32/boards/PYBV11/mpconfigboard.h
+++ ports/stm32/boards/PYBV11/mpconfigboard.h
@@ -30,8 +30,8 @@
 #define MICROPY_HW_UART1_RX     (pin_A10)
 #define MICROPY_HW_UART2_TX     (pin_A2)
 #define MICROPY_HW_UART2_RX     (pin_A3)
-#define MICROPY_HW_UART2_RTS    (pin_A1)
-#define MICROPY_HW_UART2_CTS    (pin_A0)
+//#define MICROPY_HW_UART2_RTS    (pin_A1)
+//#define MICROPY_HW_UART2_CTS    (pin_A0)
 //#define MICROPY_HW_UART3_NAME   "YB"
 //#define MICROPY_HW_UART3_TX     (pin_B10)//disable
 //#define MICROPY_HW_UART3_RX     (pin_B11)
@@ -46,11 +46,11 @@
 
 // I2C buses correct
 #define MICROPY_HW_I2C1_NAME "X"
-#define MICROPY_HW_I2C1_SCL (pin_B6)//b10
-#define MICROPY_HW_I2C1_SDA (pin_B7)//b11
+#define MICROPY_HW_I2C1_SCL (pin_B10)//b10
+#define MICROPY_HW_I2C1_SDA (pin_B11)//b11
 #define MICROPY_HW_I2C2_NAME "Y"
-#define MICROPY_HW_I2C2_SCL (pin_B10)//b8
-#define MICROPY_HW_I2C2_SDA (pin_B11)//b9
+#define MICROPY_HW_I2C2_SCL (pin_B8)//b8
+#define MICROPY_HW_I2C2_SDA (pin_B9)//b9
 
 // SPI buses correct
 #define MICROPY_HW_SPI1_NAME "X"
@@ -58,7 +58,7 @@
 #define MICROPY_HW_SPI1_SCK  (pin_A5) // X6
 #define MICROPY_HW_SPI1_MISO (pin_A6) // X7
 #define MICROPY_HW_SPI1_MOSI (pin_A7) // X8
-#define MICROPY_HW_SPI2_NAME "Y"
+// #define MICROPY_HW_SPI2_NAME "Y"
 //#define MICROPY_HW_SPI2_NSS  (pin_B12) // Y5
 //#define MICROPY_HW_SPI2_SCK  (pin_B13) // Y6
 //#define MICROPY_HW_SPI2_MISO (pin_B14) // Y7
@@ -68,12 +68,12 @@
 #define MICROPY_HW_I2S2      (1)
 
 // CAN buses
-#define MICROPY_HW_CAN1_NAME "YA"
-#define MICROPY_HW_CAN1_TX   (pin_B9) // Y4
-#define MICROPY_HW_CAN1_RX   (pin_B8) // Y3
-#define MICROPY_HW_CAN2_NAME "YB"
-#define MICROPY_HW_CAN2_TX   (pin_B13) // Y6
-#define MICROPY_HW_CAN2_RX   (pin_B12) // Y5
+// #define MICROPY_HW_CAN1_NAME "YA"
+// #define MICROPY_HW_CAN1_TX   (pin_B9) // Y4
+// #define MICROPY_HW_CAN1_RX   (pin_B8) // Y3
+// #define MICROPY_HW_CAN2_NAME "YB"
+// #define MICROPY_HW_CAN2_TX   (pin_B13) // Y6
+// #define MICROPY_HW_CAN2_RX   (pin_B12) // Y5
 
 // USRSW has no pullup or pulldown, and pressing the switch makes the input go low
 #define MICROPY_HW_USRSW_PIN        (pin_B3)
@@ -98,8 +98,8 @@
 
 // USB config
 #define MICROPY_HW_USB_FS              (1)
-//#define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
-//#define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
+#define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
+#define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
 
 // MMA accelerometer config
 #define MICROPY_HW_MMA_AVDD_PIN     (pin_B5)
