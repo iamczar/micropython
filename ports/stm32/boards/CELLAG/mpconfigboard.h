89657c98de895f5342c510e70fe354cd8fe9a6c8
czar <czarbalangue@gmail.com>
2024-10-25 21:59:28 +0100

    added cellag folder

--- ports/stm32/boards/CELLAG/mpconfigboard.h
+++ ports/stm32/boards/CELLAG/mpconfigboard.h
@@ -1,4 +1,4 @@
-#define MICROPY_HW_BOARD_NAME       "PYBv1.1"
+#define MICROPY_HW_BOARD_NAME       "CellAgv0.7"
 #define MICROPY_HW_MCU_NAME         "STM32F405RG"
 
 #define MICROPY_HW_HAS_SWITCH       (1)
